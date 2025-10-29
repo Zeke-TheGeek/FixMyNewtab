f784d0fd5e652771bfd22064b790ebea7250c7f1
ZekeTheGeek <s300141923@ddsbstudent.ca>
2025-10-29 10:20:13 -0400

    Added blocking for post-lunch classes

--- replacetab.js
+++ replacetab.js
@@ -6,7 +6,7 @@ const currentURL = window.location.href
 d.getMinutes();
 if (currentURL.includes("ddsb.ca")) {
 	window.location.replace("https://zekestudios.ca/secret/Tabs/ZekeNewTab/index.html");
-} else if (currentHour < 1035 && currentURL.includes("youtube.com")) {
+} else if (currentHour < 1035 || currentHour > 1120 && currentURL.includes("youtube.com")) {
 	
 	chrome.runtime.sendMessage({ action: "getTabInfo" }, function(response) {});
 	
